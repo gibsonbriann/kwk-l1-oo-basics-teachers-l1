530b9bb7af5d63eaef8703f07bc20f2c6d7df034
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-22 15:32:54 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,14 +4,13 @@ class Book
   #getter + setter
   attr_accessor :author :page_count :genre
   
+  #getter
+  attr_reader :title
   
   def initialize(title)
     @title = title
   end
   
-  def title
-    @title
-  end
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
